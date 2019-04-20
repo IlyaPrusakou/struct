17a1e246fe30fe993c6cb8acbb6ab226b5368b04
Ilya Prusakou <beyourself@tut.by>
2019-04-21 02:50:38 +0400

    spectypes

--- struct/Program.cs
+++ struct/Program.cs
@@ -8,8 +8,38 @@ namespace @struct
 {
     class Program
     {
+        public struct Rectangle
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+            public int Xcoord { get; set; }
+            public int Ycoord { get; set; }
+        }
+
+        public static void CheckDublicate(Rectangle[] massive)
+        {
+            int count = 0;
+            for (int i = 1; i < massive.Length; i++)
+            {
+                if (massive[i-1].Equals(massive[i])) { count = count + 1; }
+            }
+            Console.WriteLine(count);
+        }
+            
         static void Main(string[] args)
         {
+            Rectangle Rect = new Rectangle();
+            Random rnd = new Random(10);
+            Rectangle[] RectMass = new Rectangle[100];
+            for (int i = 0; i < 99; i++)
+            {
+                RectMass[i] = new Rectangle {Height = rnd.Next(0, 3), Width = rnd.Next(0,3) };
+            }
+            //if struct, the method has found some(10 - 15 dublicats),
+            //if class, the amount of dublicats is empty.
+            CheckDublicate(RectMass);
+            Console.ReadLine();
         }
+
     }
 }
